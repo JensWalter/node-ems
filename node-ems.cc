8a7280b753ad214864e0abdbb949ae4b1c3aae92
Jens Walter <js.walter@gmx.net>
2016-06-20 18:11:59 +0200

    pretty print

--- node-ems.cc
+++ node-ems.cc
@@ -1,12 +1,12 @@
-// hello.cc
+// node-ems.cc
 #include <node.h>
 #include <tibems/tibems.h>
 #include <tibems/emsadmin.h>
-#  include <pthread.h>
-#  include <errno.h>
-#  define THREAD_RETVAL         void*
-#  define THREAD_OBJ            pthread_t
-#  define THREAD_CREATE(t,f,a)  pthread_create(&(t),NULL,(f),(a))
+#include <pthread.h>
+#include <errno.h>
+#define THREAD_RETVAL         void*
+#define THREAD_OBJ            pthread_t
+#define THREAD_CREATE(t,f,a)  pthread_create(&(t),NULL,(f),(a))
 
 
 namespace nodeems {
@@ -31,10 +31,10 @@ void sendMsgToDestinationSync(bool useTopic,const FunctionCallbackInfo<Value>& a
   // Check the number of arguments passed.
   if (args.Length() != 6) {
     // Throw an Error that is passed back to JavaScript
-    isolate->ThrowException(Exception::TypeError(
-        String::NewFromUtf8(isolate, "Wrong number of arguments")));
+    isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments")));
     return;
   }
+
   tibems_status status = TIBEMS_OK;
   const char* str    = NULL;
   tibemsErrorContext errorContext = NULL;
@@ -49,134 +49,119 @@ void sendMsgToDestinationSync(bool useTopic,const FunctionCallbackInfo<Value>& a
   	isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, tibemsStatus_GetText(status))));
     return;
   }
+
   factory = tibemsConnectionFactory_Create();
   if (!factory){
   	tibemsErrorContext_GetLastErrorString(errorContext, &str);
   	isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, str)));
     return;
   }
+
   String::Utf8Value tempstr(args[0]);
   const char * serverUrl = nodeems::ToCString(tempstr);
-status = tibemsConnectionFactory_SetServerURL(factory, serverUrl);
-    if (status != TIBEMS_OK) 
-    {
+  status = tibemsConnectionFactory_SetServerURL(factory, serverUrl);
+  if (status != TIBEMS_OK) {
   	tibemsErrorContext_GetLastErrorString(errorContext, &str);
   	isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, str)));
     return;
-    }
+  }
 
-String::Utf8Value tempstr2(args[1]);
+  String::Utf8Value tempstr2(args[1]);
   const char * userName = nodeems::ToCString(tempstr2);
   String::Utf8Value tempstr3(args[2]);
   const char * password = nodeems::ToCString(tempstr3);
-    status = tibemsConnectionFactory_CreateConnection(factory,&connection,
-                                                      userName,password);
-    if (status != TIBEMS_OK)
-    {
+  status = tibemsConnectionFactory_CreateConnection(factory,&connection, userName,password);
+  if (status != TIBEMS_OK){
   	tibemsErrorContext_GetLastErrorString(errorContext, &str);
   	isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, str)));
     return;
-}
+  }
 
-String::Utf8Value tempstr4(args[3]);
+  String::Utf8Value tempstr4(args[3]);
   const char * destName = nodeems::ToCString(tempstr4);
   if (useTopic){
-        status = tibemsTopic_Create(&destination,destName);
-    }else{
-status = tibemsQueue_Create(&destination,destName);
-}
-    if (status != TIBEMS_OK)
-    {
+    status = tibemsTopic_Create(&destination,destName);
+  }else{
+    status = tibemsQueue_Create(&destination,destName);
+  }
+  if (status != TIBEMS_OK){
   	tibemsErrorContext_GetLastErrorString(errorContext, &str);
   	isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, str)));
     return;
-    }
+  }
 
+  /* create the session */
+  status = tibemsConnection_CreateSession(connection, &session,TIBEMS_FALSE,TIBEMS_AUTO_ACKNOWLEDGE);
+  if (status != TIBEMS_OK){
+	tibemsErrorContext_GetLastErrorString(errorContext, &str);
+	isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, str)));
+    return;
+  }
 
-    /* create the session */
-    status = tibemsConnection_CreateSession(connection,
-            &session,TIBEMS_FALSE,TIBEMS_AUTO_ACKNOWLEDGE);
-    if (status != TIBEMS_OK)
-    {
+  /* create the producer */
+  status = tibemsSession_CreateProducer(session,&msgProducer,destination);
+  if (status != TIBEMS_OK){
   	tibemsErrorContext_GetLastErrorString(errorContext, &str);
   	isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, str)));
     return;
-    }
-
-
-    /* create the producer */
-    status = tibemsSession_CreateProducer(session,
-            &msgProducer,destination);
-    if (status != TIBEMS_OK)
-    {
-  	tibemsErrorContext_GetLastErrorString(errorContext, &str);
-  	isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, str)));
-    return;
-    }
+  }
 
   tibemsTextMsg msg =NULL;
-    status = tibemsTextMsg_Create(&msg);
-        if (status != TIBEMS_OK)
-        {
+  status = tibemsTextMsg_Create(&msg);
+  if (status != TIBEMS_OK){
   	tibemsErrorContext_GetLastErrorString(errorContext, &str);
   	isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, str)));
     return;
-        }
+  }
         
-        /* set the message text */
-        String::Utf8Value tempstr5(args[5]);
+  /* set the message text */
+  String::Utf8Value tempstr5(args[5]);
   const char * body = nodeems::ToCString(tempstr5);
-        status = tibemsTextMsg_SetText(msg,body);
-        if (status != TIBEMS_OK)
-        {
+  status = tibemsTextMsg_SetText(msg,body);
+  if (status != TIBEMS_OK){
   	tibemsErrorContext_GetLastErrorString(errorContext, &str);
   	isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, str)));
     return;
-        }
+  }
 
-        /* publish the message */
-        status = tibemsMsgProducer_Send(msgProducer,msg);
-        if (status != TIBEMS_OK)
-        {
+  /* publish the message */
+  status = tibemsMsgProducer_Send(msgProducer,msg);
+  if (status != TIBEMS_OK){
   	tibemsErrorContext_GetLastErrorString(errorContext, &str);
   	isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, str)));
     return;
-        }
+  }
         
-        //fetch message id
-        const char* msgId = NULL;
-        tibemsMsg_GetMessageID(msg,&msgId);
-Local<v8::String> msgIdTyped = String::NewFromUtf8(isolate, msgId);
+  //fetch message id
+  const char* msgId = NULL;
+  tibemsMsg_GetMessageID(msg,&msgId);
+  Local<v8::String> msgIdTyped = String::NewFromUtf8(isolate, msgId);
 
-        /* destroy the message */
-        status = tibemsMsg_Destroy(msg);
-        if (status != TIBEMS_OK)
-        {
+  /* destroy the message */
+  status = tibemsMsg_Destroy(msg);
+  if (status != TIBEMS_OK){
    	tibemsErrorContext_GetLastErrorString(errorContext, &str);
   	isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, str)));
     return;
-       }
+  }
 
-         /* destroy the destination */
-    status = tibemsDestination_Destroy(destination);
-    if (status != TIBEMS_OK)
-    {
+  /* destroy the destination */
+  status = tibemsDestination_Destroy(destination);
+  if (status != TIBEMS_OK){
    	tibemsErrorContext_GetLastErrorString(errorContext, &str);
   	isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, str)));
     return;
-   }
+  }
 
-    /* close the connection */
-    status = tibemsConnection_Close(connection);
-    if (status != TIBEMS_OK)
-    {
+  /* close the connection */
+  status = tibemsConnection_Close(connection);
+  if (status != TIBEMS_OK){
    	tibemsErrorContext_GetLastErrorString(errorContext, &str);
   	isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, str)));
     return;
-   }
+  }
 
-
-    tibemsErrorContext_Close(errorContext);
+  tibemsErrorContext_Close(errorContext);
   args.GetReturnValue().Set(msgIdTyped);
 }
 
@@ -195,4 +180,4 @@ void init(Local<Object> exports) {
 
 NODE_MODULE(addon, init)
 
-}  // namespace demo
+}  // namespace nodeems
